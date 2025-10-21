f1b49293c655aeee1026669f51bfac8d1ddb6165
Fuad Daoud <fuad.daoud@icloud.com>
2025-10-21 22:18:59 +0300

    adding run command

--- run.sh
+++ run.sh
@@ -1,1 +1,24 @@
-qemu-system-x86_64 -machine q35 -enable-kvm -cpu host -smp 8 -m 10G -drive file=/home/fuad/ubuntu/ubuntu.qcow2,format=qcow2,if=virtio -vga virtio -display gtk,gl=on -netdev user,id=net0 -device virtio-net,netdev=net0
+qemu-system-x86_64 -enable-kvm \
+  -m 16384 -smp 4 \
+  -hda ubuntu.qcow2 \
+  -vga qxl \
+  -device virtio-serial-pci \
+  -spice port=5930,disable-ticketing=on \
+  -device virtserialport,chardev=spicechannel0,name=com.redhat.spice.0 \
+  -chardev spicevmc,id=spicechannel0,name=vdagent \
+  -virtfs local,path=/home/fuad/progressoft_shared,mount_tag=hostshare,security_model=mapped-xattr
+
+
+# commands to enable sharing on the guest
+# sudo mkdir /mnt/shared
+# sudo mount -t 9p -o trans=virtio hostshare /mnt/shared -oversion=9p2000.L
+# ```
+#
+# Make it permanent by adding to `/etc/fstab`:
+# ```
+# hostshare /mnt/shared 9p trans=virtio,version=9p2000.L,rw 0 0
+#
+#
+#
+# open UI from host using 
+# remote-viewer spice://localhost:5930
